b2668c14ede3489a43354896843105415060a83b
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-09-05 19:35:54 -0400

    Login cambiado, sistema de Guard y Apartado Reestablecer contrasena

---
+++ src/app/helpers/guards/conductor.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivateFn, Router } from '@angular/router';
+
+export const conductorGuard: CanActivateFn = (route, state) => {
+  const router = new Router();
+  const loggedUser = JSON.parse(localStorage.getItem("loggedUser")||"[]");
+
+  if(loggedUser.esConductor){
+    return true;
+  }
+  router.navigate(['/register'])
+  return false;
+};
---
+++ src/app/pages/reestablecer-contrasena/reestablecer-contrasena.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ReestablecerContrasenaPageRoutingModule } from './reestablecer-contrasena-routing.module';
+
+import { ReestablecerContrasenaPage } from './reestablecer-contrasena.page';
+import { SharedModule } from "../../components/shared/shared.module";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReestablecerContrasenaPageRoutingModule,
+    SharedModule
+],
+  declarations: [ReestablecerContrasenaPage]
+})
+export class ReestablecerContrasenaPageModule {}
