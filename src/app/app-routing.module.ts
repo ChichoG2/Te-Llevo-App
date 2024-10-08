30535f6d722815e12a0d05f8bfe2467bd14dc1d0
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-10-07 21:39:11 -0300

    Firebase implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { conductorGuard } from './helpers/guards/conductor.guard';
+import { loginGuard } from './helpers/guards/login.guard';
 
 const routes: Routes = [
   {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBTMkHzjk0V2ZDdm2wMffwlJwM8A6Tlmps",
+    authDomain: "te-llevo-app-7297e.firebaseapp.com",
+    projectId: "te-llevo-app-7297e",
+    storageBucket: "te-llevo-app-7297e.appspot.com",
+    messagingSenderId: "649103005344",
+    appId: "1:649103005344:web:94131007ff0d14279b5bc5"
+  },
 };
 
 /*
