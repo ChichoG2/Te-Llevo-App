9f6765889b7befb87e96f4d5c3af664fe8351673
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-10-11 00:14:12 -0300

    Register con Firebase

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
+import { CrudFirebaseService } from 'src/app/helpers/services/crud-firebase.service';
 import { Usuario } from 'src/app/helpers/Usuario';
 
 @Component({
@@ -12,13 +13,48 @@ export class RegisterPage implements OnInit {
   contrasena!: string;
   esConductor: boolean = false;
 
-  constructor(private navCtr: NavController) { }
+  constructor(private navCtr: NavController,
+    private crudServ: CrudFirebaseService,
+    private toasCtrl: ToastController
+  ) { }
+
+  nuevoUsuario: any = { nombre: "", contrasena: "", esConductor: null }
+  lista: any[] = []
 
   ngOnInit() {
     null;
   }
 
-  registrar(){
+  registrarBD() {
+    this.crudServ.listarItems("Usuarios").subscribe((data: any[]) => {
+      const usuario = data.find(usuario => usuario.nombre === this.nuevoUsuario.nombre)
+
+      if (!usuario) {
+        this.crudServ.crearItem("Usuarios", this.nuevoUsuario).
+          then(() => {
+            alert("Se grabo")
+          }).
+          catch((error: string) => {
+            console.error("error: " + error)
+          })
+      } else {
+        this.mostarMensaje("El nombre ya existe!, elija otro nombre.")
+      }
+    })
+
+  }
+
+  async mostarMensaje(mensaje:string){
+    const toast = await this.toasCtrl.create({
+      message:mensaje,
+      duration:2000,
+      position:'bottom',
+      color: 'danger'
+    });
+    toast.present();
+  }
+
+  registrar() {
     let arreglo: Usuario[] = [];
     let cantUsuarios: Usuario[] = JSON.parse(localStorage.getItem("Usuarios") || "[]");
 
@@ -28,7 +64,7 @@ export class RegisterPage implements OnInit {
     user.contrasena = this.contrasena;
     user.esConductor = this.esConductor;
 
-    if(localStorage.getItem("Usuarios")){
+    if (localStorage.getItem("Usuarios")) {
       arreglo = JSON.parse(localStorage.getItem("Usuarios") ?? "");
     } else {
       arreglo = [];
@@ -38,6 +74,6 @@ export class RegisterPage implements OnInit {
     localStorage.setItem("Usuarios", JSON.stringify(arreglo));
     setTimeout(() => {
       this.navCtr.navigateForward(["/login"]);
-    },2000)
+    }, 2000)
   }
 }
