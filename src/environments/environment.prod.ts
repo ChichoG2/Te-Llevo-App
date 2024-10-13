8de3847a0e815f10cf22a61dd233cb092a3bf5cb
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-10-13 01:45:01 -0300

    Firebase en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBTMkHzjk0V2ZDdm2wMffwlJwM8A6Tlmps",
+    authDomain: "te-llevo-app-7297e.firebaseapp.com",
+    projectId: "te-llevo-app-7297e",
+    storageBucket: "te-llevo-app-7297e.appspot.com",
+    messagingSenderId: "649103005344",
+    appId: "1:649103005344:web:94131007ff0d14279b5bc5"
+  },
 };
