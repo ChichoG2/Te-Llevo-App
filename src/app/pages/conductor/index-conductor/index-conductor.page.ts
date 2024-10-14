9d7ef54c7af9ec67f1b56c23b6ad143b1a631e6e
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-10-14 16:56:56 -0300

    Index conductor BD

--- src/app/pages/conductor/index-conductor/index-conductor.page.ts
+++ src/app/pages/conductor/index-conductor/index-conductor.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/helpers/services/auth.service';
+import { CrudFirebaseService } from 'src/app/helpers/services/crud-firebase.service';
 import { Usuario } from 'src/app/helpers/Usuario';
 
 @Component({
@@ -8,12 +9,41 @@ import { Usuario } from 'src/app/helpers/Usuario';
   styleUrls: ['./index-conductor.page.scss'],
 })
 export class IndexConductorPage implements OnInit {
+  listadoItems!: any[]
   user!: any;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+    private crudServ: CrudFirebaseService
+  ) { }
 
   ngOnInit() {
     this.user = this.auth.getUser()
+    this.listar()
   }
 
+  listar() {
+    const userId = this.auth.getUser().id;
+    this.crudServ.listarItems("Viajes").subscribe((data: any[]) => {
+      const filtrarViajes = data.filter(viaje => 
+        viaje.conductor && viaje.conductor.id === userId
+      );
+      // Mapeo los viajes para formatear la hora
+      this.listadoItems = filtrarViajes.map(viaje => {
+        // Convertir el timestamp a Date
+        const timestamp = viaje.hora;
+        const date = timestamp.toDate();  
+        
+        // Formatear la hora
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        const formattedHour = (hours % 12 || 12) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + ampm;
+        
+        return {
+          ...viaje,  // Mantener el resto de los campos del viaje
+          formattedHour  // Agregar la hora formateada
+        };
+      });
+    });
+  }
 }
