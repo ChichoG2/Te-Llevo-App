b3750875e64adce1cc9a82f15b86bfb2e384cb46
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-10-14 16:43:05 -0300

    Listar viajes conductor BD

--- src/app/pages/conductor/listar-viaje-conductor/listar-viaje-conductor.page.ts
+++ src/app/pages/conductor/listar-viaje-conductor/listar-viaje-conductor.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/helpers/services/auth.service';
+import { CrudFirebaseService } from 'src/app/helpers/services/crud-firebase.service';
 
 @Component({
   selector: 'app-listar-viaje-conductor',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-viaje-conductor.page.scss'],
 })
 export class ListarViajeConductorPage implements OnInit {
+  listadoItems!: any[];
 
-  constructor() { }
+  constructor(
+    private crudServ: CrudFirebaseService,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+    this.listar()
   }
 
+  listar(){
+    const userId = this.auth.getUser().id;
+    this.crudServ.listarItems("Viajes").subscribe((data:any[]) => {
+      const filtrarViajes = data.filter(viaje => 
+        viaje.conductor && viaje.conductor.id === userId
+      );
+      this.listadoItems = filtrarViajes;
+    })
+  }
 }
