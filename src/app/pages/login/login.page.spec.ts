7ccc3dfde79b39a5cf3c375bf6c76c89dec39eb0
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-11-18 15:20:22 -0300

    login spec test

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,17 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { LoginPage } from './login.page';
-
 describe('LoginPage', () => {
-  let component: LoginPage;
-  let fixture: ComponentFixture<LoginPage>;
+  const validUser = { nombre: 'antonymus', contrasena: 'anonymus' };
+  const invalidUser = { nombre: 'wrongUser', contrasena: 'wrongPassword' };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    cy.visit('/login');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Deberia de mostrar error por credenciales incorrectas', () => {
+    // Ingresar usuario y contraseña incorrectos
+    cy.get('ion-input[placeholder="Ingrese Usuario"]').type(invalidUser.nombre);
+    cy.get('ion-input[placeholder="Ingrese Contraseña"]').type(invalidUser.contrasena);
+    cy.contains('Inicia Sesión').click();
+    
+    cy.get('ion-toast').should('exist').shadow().contains('.toast-message','Credenciales incorrectas!');
+  });
+
+  it('Deberia ir a index-conductor por credenciales validas de conductor', () => {
+    // Simular un conductor válido con mock de respuesta
+    cy.intercept('GET', '**/Usuarios', (req) => {
+      req.reply([{ nombre: validUser.nombre, contrasena: validUser.contrasena, esConductor: true }]);
+    });
+
+    // Ingresar usuario y contraseña válidos
+    cy.get('ion-input[placeholder="Ingrese Usuario"]').type(validUser.nombre);
+    cy.get('ion-input[placeholder="Ingrese Contraseña"]').type(validUser.contrasena);
+    cy.contains('Inicia Sesión').click();
+
+    // Validar navegación y mensaje
+    cy.url().should('include', '/index-conductor');
+    cy.get('ion-toast').should('exist').shadow().contains('.toast-message','Bienvenido Conductor!');
   });
 });
