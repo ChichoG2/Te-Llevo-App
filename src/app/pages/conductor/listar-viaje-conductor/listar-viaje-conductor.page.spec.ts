6aedd6c5d349da90f15492aaa689b467a38346c1
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-11-18 23:32:39 -0300

    eliminar viaje conductor spec test

--- src/app/pages/conductor/listar-viaje-conductor/listar-viaje-conductor.page.spec.ts
+++ src/app/pages/conductor/listar-viaje-conductor/listar-viaje-conductor.page.spec.ts
@@ -1,17 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ListarViajeConductorPage } from './listar-viaje-conductor.page';
-
 describe('ListarViajeConductorPage', () => {
-  let component: ListarViajeConductorPage;
-  let fixture: ComponentFixture<ListarViajeConductorPage>;
+  const validUser = { nombre: 'antonymus', contrasena: 'anonymus' };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListarViajeConductorPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    cy.visit('/login');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Debería eliminar un viaje al azar', () => {
+    // Paso 1: Ingresar credenciales y loguearse
+    cy.get('ion-input[placeholder="Ingrese Usuario"]').type(validUser.nombre);
+    cy.get('ion-input[placeholder="Ingrese Contraseña"]').type(validUser.contrasena);
+    cy.contains('Inicia Sesión').click();
+
+    // Validar navegación a la página del conductor
+    cy.url().should('include', '/index-conductor');
+
+    // Paso 2: Ir a la página de lista de viajes del conductor
+    cy.contains('Ver Viajes').click();
+    cy.url().should('include', '/listar-viaje-conductor');
+
+    // Paso 3: Capturar la lista de viajes y seleccionar uno al azar
+    cy.get('ion-item').should('have.length.greaterThan', 0);
+    cy.get('ion-item')
+      .then(($items) => {
+        const randomIndex = Math.floor(Math.random() * $items.length);
+        return cy.wrap($items[randomIndex]);
+      })
+      .within(() => {
+        // Clic en el botón "Cancelar"
+        cy.get('.danger-button').click();
+      });
+
+    // Paso 4: Confirmar la eliminación del viaje en el modal de confirmación
+    cy.get('ion-alert').should('be.visible');
+    cy.get('ion-alert button').contains('Sí').click();
+
+    cy.get('ion-toast').should('exist').shadow().contains('.toast-message','Se elimino correctamente!');
   });
 });
