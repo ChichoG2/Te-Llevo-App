bb9e649c9adf3c4ebb1fc19e7ad2374c82ef4054
Chicho <135070000+ChichoG2@users.noreply.github.com>
2024-11-18 22:27:23 -0300

    reset pass spec test

--- src/app/pages/reestablecer-contrasena/reestablecer-contrasena.page.spec.ts
+++ src/app/pages/reestablecer-contrasena/reestablecer-contrasena.page.spec.ts
@@ -1,17 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReestablecerContrasenaPage } from './reestablecer-contrasena.page';
-
 describe('ReestablecerContrasenaPage', () => {
-  let component: ReestablecerContrasenaPage;
-  let fixture: ComponentFixture<ReestablecerContrasenaPage>;
+  const validUser = { nombre: 'Verstappen', contrasena: '1234' };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ReestablecerContrasenaPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    cy.visit('/reestablecer-contrasena');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Deberia de cambiar contrasena e ir a login', () => {
+    cy.get('ion-input[placeholder="Ingrese Usuario"]').type(validUser.nombre);
+    cy.get('ion-input[placeholder="Ingrese Nueva Contraseña"]').type(validUser.contrasena);
+    cy.contains('Reestablecer Contraseña').click();
+    
+    cy.get('ion-toast').should('exist').shadow().contains('.toast-message','Contrasena cambiada correctamente!');
+    cy.url().should('include', '/login');
   });
 });
